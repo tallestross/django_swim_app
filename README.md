a6da36dc02d3775078c377bea6b3dbd8c6bb5687
tallestross <ross@maude.org.uk>
2017-10-28 08:36:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# django_swim_app
+hobby coding to allow parents to automate the sending of gala applications
---
+++ swimenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ross/Desktop/Python/Django/swimapp/swimenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
